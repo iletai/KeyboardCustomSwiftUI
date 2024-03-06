9644954c4e0c911c0119872a8b823532e20279c9
Tài Lê <iletai@hotmail.com>
2024-03-06 14:01:45 +0700

    Initial commit

---
+++ CustomKeyBoard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomKeyBoard
+//
+//  Created by Lê Quang Trọng Tài on 3/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomKeyBoard/CustomKeyBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomKeyBoardApp.swift
+//  CustomKeyBoard
+//
+//  Created by Lê Quang Trọng Tài on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomKeyBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
